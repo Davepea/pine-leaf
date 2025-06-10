5fcc663b22953ae2948e16395628586cc5042e1c
Davepea <dev.peacedave@gmail.com>
2025-06-10 07:44:14 -0700

    peace

--- app/(realtor)/dashboard/page.tsx
+++ app/(realtor)/dashboard/page.tsx
@@ -4,13 +4,15 @@ import BalanceCard from '@/components/realtor/dashboard/BalanceCard'
 import Properties from '@/components/realtor/Properties'
 import Header from '@/components/realtor/Header'
 import DashboardTable from '@/components/realtor/table/DashboardTable'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fetchEachUser } from '@/utils/axiosInstance'
 import { useRouter } from 'next/navigation'
 import { useAuth } from '@/app/context/AuthContext'
+import { MdSearch } from 'react-icons/md'
 
 const RealtorDashboard = () => {
     // const [user, setUser] = React.useState(null)
+    const [searchTerm, setSearchTerm] = useState('');
     const {setProfile} = useAuth()
     const router = useRouter()
     useEffect(()=>{
@@ -35,7 +37,13 @@ const RealtorDashboard = () => {
         <div>
             <div className="md:px-10 px-6 w-full h-screen overflow-y-scroll">
                 <Header />
-                <div className="py-5 flex flex-col gap-10">
+                <div className="md:py-5 py-[10px] flex flex-col gap-[36px] ">
+                         <form onSubmit={(e) => e.preventDefault()} className='h-[50px] md:w-full w-full md:hidden block' >
+                                            <label htmlFor="search" className='md:px-5 px-3 border border-[#2F5318]/20 h-full w-full rounded-[10px] flex items-center text-[#000000]/40'>
+                                                <input type="text" name="search" id="search" placeholder='Search transactions' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className='h-[45px] w-full outline-none border-none placeholder:text-[#000000]/80' />
+                                                <MdSearch size={22} />
+                                            </label>
+                        </form>
                     <BalanceCard />
                     <div className="flex flex-col gap-4">
                         <h3 className='text-[#000000]/80 text-xl font-bold'>Transactions</h3>
